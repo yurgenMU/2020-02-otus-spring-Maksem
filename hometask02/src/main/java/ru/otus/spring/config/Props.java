7ec7ee116fe33f7801cf37b50ca06da14a8b6dd1
yurgenMU <yurgen-95@mail.ru>
2020-03-15 14:51:05 +0300

    dead code removal

--- hometask02/src/main/java/ru/otus/spring/config/Props.java
+++
@@ -1,7 +0,0 @@
-//package ru.otus.spring.config;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//
-//@ConfigurationProperties(prefix="questions")
-//public class Props {
-//}
