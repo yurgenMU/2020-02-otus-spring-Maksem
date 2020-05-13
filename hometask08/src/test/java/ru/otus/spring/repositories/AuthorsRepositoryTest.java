4932f0acc2b0b64ad7ecbeb75a4b2e12550c7268
maksem_va <maksem.va@outlook.com>
2020-05-13 18:05:03 +0300

    unit tests added

--- hometask08/src/test/java/ru/otus/spring/repositories/AuthorsRepositoryTest.java
+++ hometask08/src/test/java/ru/otus/spring/repositories/AuthorsRepositoryTest.java
@@ -1,39 +1,36 @@
 package ru.otus.spring.repositories;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.ComponentScan;
 import ru.otus.spring.AbstractRepositoryTest;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.domain.Genre;
+import ru.otus.spring.repository.AuthorRepository;
 import ru.otus.spring.repository.BookRepository;
-import ru.otus.spring.repository.GenreRepository;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static ru.otus.spring.util.TestUtils.ID_1;
 
 @ComponentScan("ru.otus.spring.events")
-public class GenresRepositoryTest extends AbstractRepositoryTest {
+public class AuthorsRepositoryTest extends AbstractRepositoryTest {
 
     @Autowired
-    private GenreRepository genreRepository;
+    private AuthorRepository authorRepository;
 
     @Autowired
     private BookRepository bookRepository;
 
     @Test
-    void removal_books_while_genre_removal_test() {
+    void removal_books_while_author_removal_test() {
         List<Book> books = bookRepository.findAll();
-        assertTrue(books.stream().anyMatch(book -> book.getGenres().stream().map(Genre::getId)
-                .anyMatch(x -> x.equals(ID_1))));
-        genreRepository.deleteById(ID_1);
+        assertTrue(books.stream().anyMatch(book -> book.getAuthor().getId().equals(ID_1)));
+        authorRepository.deleteById(ID_1);
         books = bookRepository.findAll();
-        assertTrue(books.stream().anyMatch(book -> book.getGenres().stream().map(Genre::getId)
-                .noneMatch(x -> x.equals(ID_1))));
+        assertTrue(books.stream().noneMatch(book -> book.getAuthor().getId().equals(ID_1)));
     }
-
+    
 }
